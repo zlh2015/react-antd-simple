96d933cf3ea6ef467bba4bad59550f65328417da
zlh <zlh@163.com>
2019-07-26 18:28:00 +0800

    move editor

---
+++ src/pages/email/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ViewerPage = (props) => {
+  return (
+    <h1>home page</h1>
+  );
+}
+
+export default ViewerPage;
---
+++ src/pages/email/index.js
@@ -0,0 +1,10 @@
+import HomePage from './home';
+import withThCcLayout from '../../enhancer/withLayout'
+
+const WithThCcLayoutHomePage = withThCcLayout(HomePage);
+
+
+export {
+  HomePage,
+  WithThCcLayoutHomePage
+} 
